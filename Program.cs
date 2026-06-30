d5584f640142838535b5fc7ed4eceb4f97db41a7
cristhiancastro204 <cristhiancastro204@gmail.com>
2026-06-29 21:24:51 -0300

    Fix compiler error in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 using System.Globalization;
 using Microsoft.EntityFrameworkCore;
-
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 using ObligatorioApiario.Data;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
